9a775da74284a5e851cd395d65f6575f4170c77a
Nihal saran <nihalsarandasd@gmail.com>
2023-12-18 16:31:43 +0530

    modified:   src/content/information.js 	modified:   src/content/projects.js

--- src/content/projects.js
+++ src/content/projects.js
@@ -5,7 +5,7 @@ const projects = [
     description:
       "An App which facilitates Study materials and other imp resources of a particular college to their students ",
     stack: ["React-native", "Firebase", "Ant-design", "Figma"],
-    sourceCode: "https://github.com/randilt/Learinfy",
+    sourceCode: "https://github.com/nihalsaran/Learnify",
     img: "/Home.jpeg",
     preview: "",
   },
