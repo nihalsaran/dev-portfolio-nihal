0671ccd95610cba30de1bb02ebf3a6d294fb2b36
Nihal saran das duggirala <92871743+nihalsaran@users.noreply.github.com>
2024-05-26 18:38:50 +0530

    pics

--- src/content/projects.js
+++ src/content/projects.js
@@ -24,7 +24,7 @@ const projects = [
       "EcoRide is your ultimate solution to the pressing challenges of modern commuting. With EcoRide, we're tackling traffic congestion head-on by connecting like-minded individuals on the same route, transforming your daily journey into a shared experience.",
     stack: ["React-native", "Firebase", "NodeJS", "ExpressJS", "Figma"],
     sourceCode: "https://github.com/SajalSatsangi/Exoplanet-Prediction",
-    img: "/group.jpeg",
+    img: "/dev-portfolio-nihal/group.jpeg",
     preview: "",
   },
   {
@@ -33,7 +33,7 @@ const projects = [
       "Legal-Link-India e-Marketplace is a cutting-edge online platform revolutionizing the legal service industry in India. Our platform seamlessly connects individuals and businesses with a network of trusted legal professionals, including Advocates, Arbitrators, Mediators, Notaries, Document Writers, and more.",
     stack: ["React", "MongoDB", "NodeJS", "ExpressJS", "Figma"],
     sourceCode: "https://github.com/nihalsaran/Exoplanet-Prediction",
-    img: "/legallinkindia.png",
+    img: "/dev-portfolio-nihal/legallinkindia.png",
     preview: "",
   },
   {
@@ -42,7 +42,7 @@ const projects = [
       "A sales model which predicts the Sales forecast upon user/company data input",
     stack: ["React", "MongoDB", "NodeJS", "ExpressJS", "Figma"],
     sourceCode: "https://github.com/nihalsaran/Exoplanet-Prediction",
-    img: "/sales-graph.jpeg",
+    img: "/dev-portfolio-nihal/sales-graph.jpeg",
     preview: "",
   },
   {
@@ -51,7 +51,7 @@ const projects = [
       "An Mobile App which gives left or right indicator of the vehicle by using the accelerometer and gyroscope sensors of the mobile phone and also by using AI and ML models.",
     stack: ["React", "MongoDB", "NodeJS", "ExpressJS", "Figma"],
     sourceCode: "https://github.com/nihalsaran/Exoplanet-Prediction",
-    img: "/group.jpeg",
+    img: "/dev-portfolio-nihal/group.jpeg",
     preview: "",
   },
   {
@@ -60,7 +60,7 @@ const projects = [
       "College Website which is responsive and can be used by any college or school",
     stack: ["React", "Firebase","Figma"],
     sourceCode: "https://github.com/pri007shu/DEIpagedetd",
-    img: "/group.jpeg",
+    img: "/dev-portfolio-nihal/group.jpeg",
     preview: "https://pri007shu.github.io/DEIpagedetd/",
   },
   
